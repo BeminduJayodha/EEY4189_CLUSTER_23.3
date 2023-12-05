77901b2ee76a34fee34612db67a5c6e549238bc5
hansinihpw <139895629+hansinihpw@users.noreply.github.com>
2023-12-05 12:52:43 +0530

    Update config.php

--- MindTalk/php/config.php
+++ MindTalk/php/config.php
@@ -2,7 +2,7 @@
   $hostname = "localhost";
   $username = "root";
   $password = "";
-  $dbname = "chatapp";
+  $dbname = "user_db_mindTalk";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
